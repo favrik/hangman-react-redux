6eb6d5b452d7e639057c93edfeeed50f10ed2040
Favio Manriquez Leon <favio@favrik.com>
2016-02-12 12:30:25 -0800

    Improve Misses message

--- components/Misses.js
+++ components/Misses.js
@@ -3,9 +3,10 @@ import React, { PropTypes, Component } from 'react'
 export default class Misses extends Component {
   render() {
     const misses = this.props.value.join(', ')
+    const missesMessage = this.props.value.length ? misses + '.' : 'No misses...yet!'
 
     return (
-      <div className="misses"><strong>Misses:</strong> {misses}.</div>
+      <div className="misses"><strong>Misses:</strong> {missesMessage}</div>
 
     )
   }

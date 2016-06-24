9728dbb6ff04c5590f999c7e92659d515d6249e4
Favio Manriquez Leon <favio@favrik.com>
2016-06-24 14:56:34 -0700

    Add slider monitor

--- containers/DevTools.js
+++ containers/DevTools.js
@@ -6,15 +6,18 @@ import { createDevTools } from 'redux-devtools';
 // Monitors are separate packages, and you can make a custom one
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import SliderMonitor from 'redux-slider-monitor';
 
 // createDevTools takes a monitor and produces a DevTools component
 const DevTools = createDevTools(
   // Monitors are individually adjustable with props.
   // Consult their repositories to learn about those props.
   // Here, we put LogMonitor inside a DockMonitor.
-  <DockMonitor toggleVisibilityKey='ctrl-m'
+  <DockMonitor changeMonitorKey="ctrl-m"
+               toggleVisibilityKey='ctrl-h'
                changePositionKey='ctrl-q'>
     <LogMonitor theme='tomorrow' />
+    <SliderMonitor keyboardEnabled />
   </DockMonitor>
 );
 
